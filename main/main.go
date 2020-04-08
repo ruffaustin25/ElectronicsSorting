056f7272723e89d91abe70a183699bc2e1634cd0
Austin Ruff <ruffaustin25@yahoo.com>
2020-04-07 20:19:38 -0500

    Basic index page serving

--- main/main.go
+++ main/main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
+const indexTemplatePath string = "../templates/index.html"
+
+var indexTemplateText []byte
+
 func index(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "<h1>Main Page</h1>")
+	fmt.Fprint(res, string(indexTemplateText))
 }
 
 func main() {
+	file, err := os.Open(indexTemplatePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	indexTemplateText, err = ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", index)
 
 	server := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + os.Args[1],
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
