382bd7629eaebd180bb7010163c85c05c6b86ce1
ruffaustin25 <ruffaustin25@yahoo.com>
2022-05-28 12:11:13 -0500

    Change deployment ip

--- buildconfig/buildconfig.deploy.go
+++ buildconfig/buildconfig.deploy.go
@@ -2,5 +2,5 @@
 
 package buildconfig
 
-const DatabaseURL string = "192.168.0.2:3306"
-const BaseURL string = "http://192.168.0.2:2796"
+const DatabaseURL string = "192.168.0.200:3306"
+const BaseURL string = "http://192.168.0.200:2796"
